1a8a7ba674af3e942b378f931dc6ed69ed63a66e
BrundaBR <revadibrunda@gmail.com>
2025-12-14 06:32:52 +0000

    Updated the counter logic

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 
-let strin  = "brunda";
 
-console.log("string reveres:"+strin.split("").reverse().join(""));
-let coount =1;
-let classhtml = document.getElementsByClassName("counter")[0].innerHTML= coount++;
-console.log("classhtml:"+classhtml);
+console.log("Javascript console !");
+
+// counter 
+let count = Number(localStorage.getItem("count"))||0;
+//get element
+let classhtml = document.getElementsByClassName("counter");
+// check for refresh 
+const navigationType = window.performance.getEntriesByType('navigation')[0].type;
+
+if  (navigationType=="reload"){
+    console.log("Page reloaded!!");
+    // increment counter
+    
+    count++;
+    localStorage.setItem("count",count)
+    console.log("updated counter"+count);
+    
+    classhtml[0].innerHTML =  `Counter ${count}`;
+
+    console.log( classhtml[0].innerHTML);
+    
+}
+
+ 
+
 
--- readme.md
+++ readme.md
@@ -1,8 +1,10 @@
 # Counter
+- dispaly counter in home page whenever there is page refresh
+- Increment on page reloads
 
-
+What is required>
+- 
 
 # Weather APIs
 
 
-# 
