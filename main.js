ddc61691569ba1be74184f0560092e2ae462a046
BrundaBR <revadibrunda@gmail.com>
2025-12-14 04:55:35 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let strin  = "brunda";
+
+console.log("string reveres:"+strin.split("").reverse().join(""));
+let coount =1;
+let classhtml = document.getElementsByClassName("counter")[0].innerHTML= coount++;
+console.log("classhtml:"+classhtml);
+
